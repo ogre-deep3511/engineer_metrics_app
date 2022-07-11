b286a25fd0dc17e7cc4bb9e2a05bf7da18a67224
Tapaj Kumar Das <tapajkumardas@gmail.com>
2022-07-12 01:36:26 +0530

    Adds basic pull request stats

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import { Table } from 'antd';
 import './App.css';
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const owner = "tapaj";
+  const repo = "test";
+
+  const [dataSource, setDataSource] = useState([]);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "PR Count",
+      dataIndex: "count",
+      key: "count"
+    },
+    {
+      title: "Additions",
+      dataIndex: "additions",
+      key: "additions"
+    },
+    {
+      title: "Deletions",
+      dataIndex: "deletions",
+      key: "deletions"
+    }
+  ];
+
+  useEffect(() => {
+
+    async function getPullRequest () {
+      let key = 0;
+      const prApiResponse = await axios.get(`https://api.github.com/repos/${owner}/${repo}/pulls?state="closed"`);
+      const prData = prApiResponse.data;
+  
+      const prCleanedData = await Promise.all(prData.map(async (pr) => {
+        const pullsUrl = pr.url;
+  
+        // Getting commit data
+        const pullApiResponse = await axios.get(pullsUrl);
+        const pullData = pullApiResponse.data;
+        key++;
+  
+        return {
+          key: key,
+          name: pr.user.login,
+          additions: pullData.additions,
+          deletions: pullData.deletions
+        }
+      }));
+      
+      // Creating a hash map from the cleaned data to combine pull requests from one person
+      const prMap = new Map();
+
+      prCleanedData.forEach(pr => {
+        if (prMap.get(pr.name) === undefined) {
+          pr.count = 1;
+          prMap.set(pr.name, pr);
+        } else {
+          const existingData = prMap.get(pr.name);
+          existingData.count++;
+          existingData.additions += pr.additions;
+          existingData.deletions += pr.deletions;
+          prMap.set(pr.name, existingData);
+        }
+      });
+
+      const consolidatedPrData = Array.from(prMap.values());
+
+      setDataSource(consolidatedPrData)
+    }
+
+    getPullRequest();
+   
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table-container">
+        <Table dataSource={dataSource} columns={columns} />
+      </div>
     </div>
   );
 }
